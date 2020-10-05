dcbf58bcae77b3e4994a963e53ef24e286a92e26
Jordan <jochsf@gmail.com>
2020-10-05 03:08:35 -0700

    added Employee constructor and test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('../lib/Employee.js');
+
+test('create employee object', () => {
+    const employee = new Employee('Jordan', 1, 'email@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining('@'))
+})
+
+test('gets name', () => {
+    const employee = new Employee('Jordan');
+
+    expect(employee.getName()).toEqual(expect.any(String))
+})
+
+test('gets id', () => {
+    const employee = new Employee('1');
+
+    expect(employee.getId()).toEqual(expect.any(Number))
+})
+
+test('gets email', () => {
+    const employee = new Employee('email@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('@'))
+})
+
+test('gets role', () => {
+    const employee = new Employee('Employee')
+
+    expect(employee.getRole()).toBe('Employee')
+})
