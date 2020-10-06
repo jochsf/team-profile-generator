db77258b16c5cf09385be66db98646842d2f827b
Jordan <Joch@Jordans-MacBook-Pro.local>
2020-10-06 01:46:02 -0700

    initial index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// libraries
+
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+// generate html page 
+const generateHTML = require('./src/generate-html')
+
+function 
